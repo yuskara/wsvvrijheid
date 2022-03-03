8cc7b58b11850f242223707e331e00a6e517fb2e
yuskara <ykaraca@gmail.com>
2022-03-03 11:23:01 -0500

    project page created

---
+++ src/pages/projects.js
@@ -0,0 +1,36 @@
+import { SimpleGrid, Container } from '@chakra-ui/react'
+import Card from '../components/Card/index'
+import projectsData from 'data/projects.json'
+import { Layout } from 'components/layout/layout'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+export default function Projects({ title, content, seo }) {
+  return (
+    <Layout scrollHeight={100} seo={seo}>
+      <Container maxW='80rem' centerContent>
+        <SimpleGrid columns={[1, 2, 1, 2]}>
+          {content.map(({ title, description, image, button, link }, i) => (
+            <Card key={i} title={title} description={description} image={image} button={button} link={link} />
+          ))}
+        </SimpleGrid>
+      </Container>
+    </Layout>
+  )
+}
+export const getStaticProps = async context => {
+  const { locale } = context
+  console.log(context)
+  const pageData = projectsData[locale]
+  console.log(pageData)
+  const seo = {
+    title: pageData.title,
+  }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      title: pageData.title,
+      content: pageData.content,
+      seo,
+    },
+  }
+}
