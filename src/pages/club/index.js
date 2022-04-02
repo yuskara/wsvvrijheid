85298285e9cc097f07f9e4cbc3b8d23f94bd7a9e
dnlrkaan <dnlrkaan@gmail.com>
2022-04-02 20:10:00 +0200

    Added

---
+++ src/pages/club/index.js
@@ -0,0 +1,55 @@
+import { Container } from '@chakra-ui/react'
+import { Box, ChakraProvider, Image } from '@chakra-ui/react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import React from 'react'
+
+// import Masonry from 'react-masonry-css'
+import { Layout, PageTitle } from '~components'
+
+export default function Club({ header, images }) {
+  return (
+    <Layout scrollHeight={100} seo={{ header }}>
+      <Container maxW='100%' centerContent>
+        <PageTitle>{header}</PageTitle>
+        <ChakraProvider>
+          <Box
+            padding={4}
+            w='100%'
+            mx='auto'
+            // bg='gray.800'
+            sx={{ columnCount: [1, 2, 3, 4, 5, 6, 7, 8], columnGap: '8px' }}
+          >
+            {images.map(img => (
+              <Image key={img.i} w='100%' borderRadius='xl' mb={2} d='inline-block' src={img.src} alt='Alt' />
+            ))}
+          </Box>
+        </ChakraProvider>
+      </Container>
+    </Layout>
+  )
+}
+
+export const getStaticProps = async context => {
+  const { locale } = context
+  let images = []
+  const imgId = [1011, 883, 1074, 823, 64, 65, 839, 314, 256, 316, 92, 643]
+  for (let i = 0; i < imgId.length; i++) {
+    const ih = 200 + Math.floor(Math.random() * 10) * 15
+    images.push({ id: i, src: 'https://unsplash.it/250/' + ih + '?image=' + imgId[i] })
+  }
+  const seo = {
+    title: {
+      en: 'Club',
+      nl: 'Club',
+      tr: 'Club',
+    },
+  }
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      header: seo.title[locale],
+      images,
+    },
+  }
+}
