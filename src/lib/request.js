14d154c2331941b98cae55ee0e23a51bd75fa9ad
7alip <talipaltas@gmail.com>
2022-04-07 12:43:19 +0200

    Remove user token from request

--- src/lib/request.js
+++ src/lib/request.js
@@ -5,19 +5,21 @@ import { transformStrapiData } from '~utils'
 
 export const fetcher = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+  },
 })
 
-export const request = async (
-  { publicationState = 'live', locale, url, filters, sort, populate = '*', page = 1, pageSize = 10 },
-  token = process.env.NEXT_PUBLIC_API_TOKEN,
-) => {
-  fetcher = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-    },
-  })
-
+export const request = async ({
+  publicationState = 'live',
+  locale,
+  url,
+  filters,
+  sort,
+  populate = '*',
+  page = 1,
+  pageSize = 10,
+}) => {
   const query = qs.stringify(
     {
       publicationState,
@@ -25,24 +27,21 @@ export const request = async (
       filters,
       sort,
       populate,
-      pagination: { page, pageSize },
+      pagination: {
+        page,
+        pageSize,
+      },
     },
-    { encodeValuesOnly: true },
+    {
+      encodeValuesOnly: true,
+    },
   )
 
+  // TODO Consider a better error handling
   try {
-    // Set the AUTH token for any request
-    fetcher.interceptors.request.use(function (config) {
-      if (token === null) config.headers.Authorization = null
-      else config.headers.Authorization = `Bearer ${token}`
-
-      return config
-    })
-
     const response = await fetcher.get(`/${url}?${query}`)
     return transformStrapiData(response.data)
   } catch (error) {
-    // TODO Consider a better error handling
     console.error(error.data || error.response || error.message)
     return null
   }
