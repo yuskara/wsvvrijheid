4cb68fab98dcf5775bc58800c80669561a7b1a54
7alip <talipaltas@gmail.com>
2022-04-09 23:59:50 +0200

    Refactor form-item for password type

--- src/components/forms/login-form.js
+++ src/components/forms/login-form.js
@@ -1,18 +1,4 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  Divider,
-  FormControl,
-  FormLabel,
-  Heading,
-  HStack,
-  Link,
-  Stack,
-  Text,
-  useBreakpointValue,
-} from '@chakra-ui/react'
+import { Box, Button, Checkbox, Container, Divider, Heading, HStack, Link, Stack, Text } from '@chakra-ui/react'
 import { yupResolver } from '@hookform/resolvers/yup'
 import axios from 'axios'
 import { useRouter } from 'next/router'
@@ -26,13 +12,12 @@ import { useUser } from '~hooks'
 
 import { FormItem } from './form-item'
 import { OAuthButtonGroup } from './oauth-button-group'
-import { PasswordField } from './password-field'
+
 const schema = t =>
   yup.object({
     password: yup
       .string()
       .min(8, t('login.password.warning'))
-      .max(32)
       .required(t('login.password.required'))
       .matches(RegExp('(.*[a-z].*)'), t('login.password.matches.lowercase'))
       .matches(RegExp('(.*[A-Z].*)'), t('login.password.matches.uppercase'))
@@ -42,15 +27,16 @@ const schema = t =>
       .string()
       .email(t`contact.form.email-invalid`)
       .required(t`login.email.required`),
-    message: yup.string().required(t`login.email.required`),
   })
 
 export const LoginForm = () => {
-  const { t } = useTranslation()
-  const [errorMessage, setErrorMessage] = useState('')
   useUser('/profile', true)
 
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const { t } = useTranslation()
   const router = useRouter()
+
   const {
     register,
     handleSubmit,
@@ -81,7 +67,7 @@ export const LoginForm = () => {
       }
     } catch (error) {
       if (error?.response?.data?.error?.message) {
-        setErrorMessage(eerror?.response?.data?.error?.message)
+        setErrorMessage(error?.response?.data?.error?.message)
         setTimeout(() => {
           setErrorMessage('')
         }, 2000)
@@ -90,12 +76,13 @@ export const LoginForm = () => {
       }
     }
   }
+
   return (
     <Container maxW='lg' py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
       <Stack spacing='8'>
         <Stack spacing='6'>
           <Stack spacing={{ base: '2', md: '3' }} textAlign='center'>
-            <Heading size={useBreakpointValue({ base: 'xs', md: 'sm' })}>{t('login.sign-in-header.title')}</Heading>
+            <Heading>{t('login.sign-in-header.title')}</Heading>
             <HStack spacing='1' justify='center'>
               <Text color='muted'>{t('login.sign-in-header.text')}</Text>
               <Button variant='link' as={Link} href='/user/register' colorScheme='blue'>
@@ -104,20 +91,19 @@ export const LoginForm = () => {
             </HStack>
           </Stack>
         </Stack>
-        <Box
-          py={{ base: '0', sm: '8' }}
-          px={{ base: '4', sm: '10' }}
-          bg={{ base: 'transparent', sm: 'bg-surface' }}
-          borderRadius={{ base: 'none', sm: 'xl' }}
-        >
+        <Box py={{ base: '0', sm: '8' }} px={{ base: '4', sm: '10' }} borderRadius={{ base: 'none', sm: 'xl' }}>
           <Stack spacing='6' as='form' onSubmit={handleSubmit(handleSubmitSign)}>
             <Stack spacing='5'>
-              {errorMessage ? <Text color='red.500'>{errorMessage}</Text> : ''}
-              <FormControl>
-                <FormLabel htmlFor='email'>{t('login.email.title')}</FormLabel>
-                <FormItem name='email' id='email' type='email' register={register} errors={errors} />
-                <PasswordField register={register} errors={errors} />
-              </FormControl>
+              {errorMessage && <Text color='red.500'>{errorMessage}</Text>}
+              <FormItem id='email' label={t('login.email.title')} type='email' register={register} errors={errors} />
+              <FormItem
+                id='password'
+                type='password'
+                label={t('login.password.title')}
+                autoComplete='current-password'
+                register={register}
+                errors={errors}
+              />
             </Stack>
             <HStack justify='space-between'>
               <Checkbox defaultIsChecked>{t('login.remember-me')}</Checkbox>
--- src/components/forms/signup-form.js
+++ src/components/forms/signup-form.js
@@ -1,17 +1,4 @@
-import {
-  Box,
-  Button,
-  Container,
-  Divider,
-  FormControl,
-  FormLabel,
-  Heading,
-  HStack,
-  Link,
-  Stack,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react'
+import { Box, Button, Container, Divider, Heading, HStack, Link, Stack, Text } from '@chakra-ui/react'
 import { yupResolver } from '@hookform/resolvers/yup'
 import axios from 'axios'
 import { useRouter } from 'next/router'
@@ -25,15 +12,14 @@ import { useUser } from '~hooks'
 
 import { FormItem } from './form-item'
 import { OAuthButtonGroup } from './oauth-button-group'
-import { PasswordField } from './password-field'
 
 const schema = t =>
   yup.object({
-    name: yup.string().required(t`login.name.required`),
+    username: yup.string().required(t`login.name.required`),
     password: yup
       .string()
-      .required(t('login.password.required'))
       .min(8, t('login.password.warning'))
+      .required(t('login.password.required'))
       .matches(RegExp('(.*[a-z].*)'), t('login.password.matches.lowercase'))
       .matches(RegExp('(.*[A-Z].*)'), t('login.password.matches.uppercase'))
       .matches(RegExp('(.*\\d.*)'), t('login.password.matches.number'))
@@ -42,14 +28,16 @@ const schema = t =>
       .string()
       .email(t`contact.form.email-invalid`)
       .required(t`login.email.required`),
-    message: yup.string().required(t`login.email.required`),
   })
 
 export const SignupForm = () => {
-  const { t } = useTranslation()
-  const [error, setErrorMessage] = useState(null)
-  const router = useRouter()
   useUser('/profile', true)
+
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const { t } = useTranslation()
+  const router = useRouter()
+
   const {
     register,
     handleSubmit,
@@ -61,14 +49,8 @@ export const SignupForm = () => {
   })
 
   const handleSubmitSignUp = async data => {
-    console.log('Data', data)
-    const body = {
-      username: data.name,
-      email: data.email,
-      password: data.password,
-    }
     try {
-      const resp = await axios.post('/api/auth/register', body)
+      const resp = await axios.post('/api/auth/register', data)
       if (resp?.data?.error) {
         setErrorMessage(resp?.data?.error?.message)
         setTimeout(() => {
@@ -81,7 +63,7 @@ export const SignupForm = () => {
       }
     } catch (error) {
       if (error?.response?.data?.error?.message) {
-        setErrorMessage(eerror?.response?.data?.error?.message)
+        setErrorMessage(error?.response?.data?.error?.message)
         setTimeout(() => {
           setErrorMessage('')
         }, 2000)
@@ -90,12 +72,13 @@ export const SignupForm = () => {
       }
     }
   }
+
   return (
     <Container maxW='lg' py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
       <Stack spacing='8'>
         <Stack spacing='6'>
           <Stack spacing={{ base: '2', md: '3' }} textAlign='center'>
-            <Heading size={{ base: 'xs', md: 'sm' }}>{t('login.sign-up-header.title')}</Heading>
+            <Heading>{t('login.sign-up-header.title')}</Heading>
             <HStack spacing='1' justify='center'>
               <Text color='muted'>{t('login.sign-up-header.text')}</Text>
               <Button variant='link' as={Link} href='/user/login' colorScheme='blue'>
@@ -104,25 +87,20 @@ export const SignupForm = () => {
             </HStack>
           </Stack>
         </Stack>
-        <Box
-          py={{ base: '0', sm: '8' }}
-          px={{ base: '4', sm: '10' }}
-          boxShadow={{ base: 'none', sm: useColorModeValue('md', 'md-dark') }}
-          borderRadius={{ base: 'none', sm: 'xl' }}
-        >
+        <Box py={{ base: '0', sm: '8' }} px={{ base: '4', sm: '10' }} borderRadius={{ base: 'none', sm: 'xl' }}>
           <Stack spacing='6' as='form' onSubmit={handleSubmit(handleSubmitSignUp)}>
             <Stack spacing='5'>
-              {error ? <Text color='red.500'>{error}</Text> : ''}
-              <FormControl>
-                <FormLabel htmlFor='name'>{t('login.name.title')}</FormLabel>
-                <FormItem name='name' id='name' type='text' register={register} errors={errors} />
-                <FormLabel htmlFor='email'>{t('login.email.title')}</FormLabel>
-                <FormItem name='email' id='email' type='email' register={register} errors={errors} />
-                <PasswordField register={register} errors={errors} />
-              </FormControl>
-              <Text fontSize='sm' whiteSpace='nowrap' color='muted'>
-                {t('login.password.warning')}
-              </Text>
+              {errorMessage && <Text color='red.500'>{errorMessage}</Text>}
+              <FormItem id='username' label={t('login.name.title')} register={register} errors={errors} />
+              <FormItem id='email' type='email' label={t('login.email.title')} register={register} errors={errors} />
+              <FormItem
+                id='password'
+                type='password'
+                label={t('login.password.title')}
+                autoComplete='current-password'
+                register={register}
+                errors={errors}
+              />
             </Stack>
             <Stack spacing='6'>
               <Button type='submit' colorScheme='blue'>
