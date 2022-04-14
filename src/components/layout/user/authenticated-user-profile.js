2a11efc78d832c09d3b85676fa4cc13ccbd8040f
Mustafa <cana66331@gmail.com>
2022-04-14 18:58:23 +0200

    feat arts data added

--- src/components/layout/user/authenticated-user-profile.js
+++ src/components/layout/user/authenticated-user-profile.js
@@ -1,12 +1,57 @@
-import { Avatar, Button, ButtonGroup, HStack, Stack, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
+import { Avatar, Button, ButtonGroup, HStack, Image, Stack, Text, VStack } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
 import { useAuth } from '~hooks'
+import { request } from '~lib'
 
 export const AuthenticatedUserProfile = () => {
   const user = useAuth()
+  const [arts, setArts] = useState(null)
+  const [showArt, setShowArt] = useState(true)
+  const [tools, setTools] = useState(false)
+  const { locale } = useRouter()
+  useEffect(() => {
+    const getData = async () => {
+      await request({
+        url: 'api/arts',
+        locale: locale,
+        filters: {
+          artist: {
+            user: {
+              id: {
+                $eq: user.id,
+              },
+            },
+          },
+        },
+        populate: ['artist.user', 'images'],
+      })
+        .then(res => setArts(res.result))
+        .catch(er => console.log('Error ', er))
+    }
+    getData()
+  }, [user.id])
 
-  console.log('User: ', user)
+  const rejected = arts?.filter(art => art.status === 'rejected')
+  const approved = arts?.filter(art => art.status === 'approved')
+
+  console.log('Arts> ', arts, '\n', 'rejected> ', rejected, 'approved; ', approved, 'user: ', user)
+
+  const showApproved = e => {
+    e.preventDefault()
+    setShowArt(true)
+    setTools(false)
+  }
+  const showRejected = e => {
+    e.preventDefault()
+    setShowArt(false)
+    setTools(false)
+  }
+  const showTools = e => {
+    e.preventDefault()
+    setTools(true)
+  }
   return (
     <Stack spacing={4} align='stretch'>
       <VStack
@@ -28,14 +73,64 @@ export const AuthenticatedUserProfile = () => {
         </HStack>
       </VStack>
 
-      <Stack direction='flex' justify='space-between'>
+      <vStack direction='flex' justify='space-between'>
         <ButtonGroup colorScheme='blue.400' variant='ghost'>
-          <Button>My arts</Button>
-          <Button>My Approved Arts</Button>
-          <Button>My Returned Arts</Button>
-          <Button>General Tools</Button>
+          <Button onClick={showApproved}>My arts</Button>
+          <Button onClick={showRejected}>My Returned Arts</Button>
+          <Button onClick={showTools}>General Tools</Button>
         </ButtonGroup>
-      </Stack>
+        <Stack>
+          {tools ? (
+            <>
+              {
+                //gereral tools here
+              }
+              <Stack>
+                <Text>Username: {user?.user?.username}</Text>
+                <Text>Email: {user?.user?.email}</Text>
+              </Stack>
+            </>
+          ) : showArt ? (
+            <HStack direction={'flex'}>
+              {
+                //Appraved Arts  here
+              }
+              {approved?.map(art => {
+                return art?.images?.map((el, index) => {
+                  return (
+                    <VStack key={index} alt='no image'>
+                      <Image src={process.env.NEXT_PUBLIC_API_URL + el.formats?.thumbnail?.url} alt='no image' />
+                      <HStack>
+                        <Text>{art.title}</Text>
+                        <Text>Likes: {art.likes}</Text>
+                      </HStack>
+                    </VStack>
+                  )
+                })
+              })}
+            </HStack>
+          ) : (
+            <HStack direction={'flex'}>
+              {
+                //Rejected Arts  here
+              }
+              {rejected?.map(art => {
+                return art?.images?.map((el, index) => {
+                  return (
+                    <VStack key={index} alt='no image'>
+                      <Image src={process.env.NEXT_PUBLIC_API_URL + el.formats?.thumbnail?.url} alt='no image' />
+                      <HStack>
+                        <Text>{art.title}</Text>
+                        <Text>Likes: {art.likes}</Text>
+                      </HStack>
+                    </VStack>
+                  )
+                })
+              })}
+            </HStack>
+          )}
+        </Stack>
+      </vStack>
     </Stack>
   )
 }
