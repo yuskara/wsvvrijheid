6fd5b9efb1629728be5fe6919d8e0c0edd8fcab3
7alip <talipaltas@gmail.com>
2022-04-16 10:20:43 +0200

    Update AuthenticatedUserProfile design

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,20 +1,13 @@
-import { Stack, StackDivider } from '@chakra-ui/react'
 import { withIronSessionSsr } from 'iron-session/next'
 import React from 'react'
 
-import { Container, Layout } from '~components'
+import { AuthenticatedUserProfile, Layout } from '~components'
 import { sessionOptions } from '~lib'
-
-import { AuthenticatedUserProfile } from '../../components/layout/user/authenticated-user-profile'
 
 const Profile = () => {
   return (
-    <Layout>
-      <Container>
-        <Stack divider={<StackDivider borderColor='gray.200' />} spacing={4} align='stretch'>
-          <AuthenticatedUserProfile />
-        </Stack>
-      </Container>
+    <Layout isDark>
+      <AuthenticatedUserProfile />
     </Layout>
   )
 }
