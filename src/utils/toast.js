02c9181b2c2438e1bf0042f92a86604b8ce0b8b0
zahirekrem09 <zhrekrmsrtk@gmail.com>
2022-04-24 11:48:26 +0200

    feat:added custom toast

---
+++ src/utils/toast.js
@@ -0,0 +1,25 @@
+import { createStandaloneToast } from '@chakra-ui/react'
+
+import { theme } from '~theme'
+
+const toast = createStandaloneToast({
+  theme,
+})
+
+/**
+ * this function is used to show toast message
+ * @param  {String} title title of the toast
+ * @param {String}  description   description of the toast
+ * @param {("info" | "warning" | "success" | "error")}  status   status of the toast
+ */
+
+export const toastMessage = (title, description, status) => {
+  toast({
+    title,
+    description,
+    status,
+    duration: 5000,
+    position: 'top-right',
+    isClosable: true,
+  })
+}
