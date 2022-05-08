56a5462a0ba5c43fdae1f45523d07f825fa8cc9a
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2022-05-08 12:42:43 +0200

    added random height to masonry grid skeleton

--- src/components/layout/skeletons/masonry-grid-skeleton.js
+++ src/components/layout/skeletons/masonry-grid-skeleton.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 export const MasonryGridSkeleton = () => {
   return (
-    <SimpleGrid mt={3} w={'200px'} m={2}>
-      <Skeleton size='5' height={'250px'} />
-      <SkeletonText mt='4' noOfLines={2} spacing='4' />
+    <SimpleGrid mt={3} m={2}>
+      <Skeleton size='5' h={Math.floor(Math.random() * (400 - 200 + 1)) + 200} />
+      <SkeletonText mt='4' noOfLines={2} />
     </SimpleGrid>
   )
 }
