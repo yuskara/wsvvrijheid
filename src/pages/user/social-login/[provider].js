b6f90791d596fa9af10ce26b2396e9e35babc5e1
7alip <talipaltas@gmail.com>
2022-05-16 09:58:37 +0200

    Add email taken statement for social login

--- src/pages/user/social-login/[provider].js
+++ src/pages/user/social-login/[provider].js
@@ -1,33 +1,79 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Button, Center, Stack } from '@chakra-ui/react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React, { useEffect, useState } from 'react'
 
-import { Layout } from '~components'
+import { Layout, Navigate } from '~components'
 import { useAuth } from '~hooks'
 
 const AuthProvider = () => {
   const [loading, setLoading] = useState(false)
+  const [emailTaken, setEmailTaken] = useState(null)
+
+  const { t } = useTranslation()
   const router = useRouter()
   useAuth('/profile', true)
-  const { query } = router
+
   useEffect(() => {
-    if (query.provider && query.access_token) {
+    setLoading(true)
+    if (router.query.provider && router.query.access_token) {
       axios
-        .post(`/api/auth/${query.provider}`, {
-          access_token: query.access_token,
+        .post(`/api/auth/${router.query.provider}`, {
+          access_token: router.query.access_token,
         })
         .then(response => {
           if (response.data.token) {
-            setLoading(true)
             router.push('/')
           }
         })
         .catch(error => {
-          console.error('error', error)
+          if (error.response?.data.message === 'Email is already taken.') {
+            setEmailTaken(true)
+          }
+
+          console.error('error', error?.response || error)
+          setLoading(false)
         })
     }
   }, [router])
-  return <Layout isLoading={loading} />
+
+  return (
+    <Layout isLoading={loading}>
+      {emailTaken && (
+        <Center h='calc(70vh)'>
+          <Alert as={Stack} status='warning' textAlign='center' py={16} w='container.sm' shadow='lg' rounded='lg'>
+            <AlertIcon boxSize='60px' mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize='2xl'>
+              {t`login.email-taken.title`}
+            </AlertTitle>
+            <AlertDescription maxWidth='sm'>{t`login.email-taken.description`}</AlertDescription>
+            <Navigate as={Button} colorScheme='blue' href='/user/login'>{t`login.sign-in`}</Navigate>
+          </Alert>
+        </Center>
+      )}
+    </Layout>
+  )
+}
+
+export const getStaticPaths = async () => {
+  const providers = ['google', 'twitter', 'facebook']
+
+  return {
+    paths: providers.map(provider => ({ params: { provider } })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async context => {
+  const locale = context.locale
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  }
 }
 
 export default AuthProvider
