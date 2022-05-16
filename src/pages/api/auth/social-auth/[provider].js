af4e9c4ef8ec2df11e35b6831a2309db932691c2
zahirekrem09 <zhrekrmsrtk@gmail.com>
2022-05-16 11:33:45 +0200

    fixed providers dynamic route

---
+++ src/pages/api/auth/social-auth/[provider].js
@@ -0,0 +1,28 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
+
+import { fetcher, sessionOptions } from '~lib'
+
+const route = async (req, res) => {
+  const { provider } = req.query
+  if (req.method === 'POST') {
+    try {
+      const response = await fetcher.get(`/api/auth/${provider}/callback?access_token=${req.body.access_token}`)
+      const auth = { ...response.data.user, token: response.data.jwt, user: response.data.user }
+      req.session = auth
+      await req.session.save()
+      res.json(auth)
+    } catch (error) {
+      console.error(error)
+      if (!error.response?.data?.error.message) {
+        return res.status(500).json({ message: 'Internal server error' })
+      } else {
+        const messages = error.response?.data?.error.message
+        return res.status(403).json({ message: messages })
+      }
+    }
+  }
+}
+
+const handler = withIronSessionApiRoute(route, sessionOptions)
+
+export default handler
