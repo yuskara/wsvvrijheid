a56a7a267561cfbefcede509b275b6e38cfad773
Mustafa <cana66331@gmail.com>
2022-05-19 13:27:49 +0200

    feat added image without background

--- src/components/home-justice.js
+++ src/components/home-justice.js
@@ -1,11 +1,20 @@
-import { Image, Text } from '@chakra-ui/react'
+import { Image, SimpleGrid, Stack, Text } from '@chakra-ui/react'
 import { useTranslation } from 'next-i18next'
 export const HomeJustice = () => {
   const { t } = useTranslation()
   return (
-    <>
-      <Image src='/images/scales-of-justice.jpg' />
-      <Text fontSize='xl'>{t`home.justice`}</Text>
-    </>
+    <Stack direction='row' bg={'transparent'}>
+      <SimpleGrid columns={{ base: 1, lg: 2 }} justifyItems='center' gap={8}>
+        <Image
+          boxSize='400px'
+          borderRadius='full'
+          objectFit='cover'
+          //src='/images/scales-of-justice.jpeg'
+          src='/images/scales-of-justice.png'
+          alt='/images/scales-of-justice.jpeg'
+        />
+        <Text fontSize='xl'>{t`home.justice`}</Text>
+      </SimpleGrid>
+    </Stack>
   )
 }
